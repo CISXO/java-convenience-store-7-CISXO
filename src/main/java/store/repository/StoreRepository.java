4066c33a1c8cf64a899a97af14a306b4d4dd7452
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-11-08 17:31:30 +0900

    feat(repository): StoreRepository feature save && get
    - save 및 get 추가

--- src/main/java/store/repository/StoreRepository.java
+++ src/main/java/store/repository/StoreRepository.java
@@ -1,6 +1,29 @@
 package store.repository;
 
+import store.domain.product.Product;
+import store.domain.product.Products;
+import store.domain.promotion.Promotions;
+
+import java.util.List;
+
 public class StoreRepository {
 
+    private Products products;
+    private Promotions promotions;
 
+    public void saveProducts(Products products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products.getProducts();
+    }
+
+    public void savePromotions(Promotions promotions) {
+        this.promotions = promotions;
+    }
+
+    public Promotions getPromotions() {
+        return promotions;
+    }
 }
