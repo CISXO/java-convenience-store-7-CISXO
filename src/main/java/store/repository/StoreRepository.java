8c2cc36cfd245167a74c461228e75921d3de0aca
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-11-29 14:46:43 +0900

    refactor(repository): service logic moves to repository

--- src/main/java/store/repository/StoreRepository.java
+++ src/main/java/store/repository/StoreRepository.java
@@ -1,8 +1,11 @@
 package store.repository;
 
+import store.domain.order.OrderItem;
 import store.domain.product.Product;
 import store.domain.product.Products;
+import store.domain.promotion.Promotion;
 import store.domain.promotion.Promotions;
+import store.global.exception.ExceptionMessage;
 
 import java.util.List;
 
@@ -19,11 +22,42 @@ public class StoreRepository {
         return products.getProducts();
     }
 
+    public Product getProducts(String productName) {
+        return products.getProducts().stream()
+                .filter(product -> product.getName().equals(productName))
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(ExceptionMessage.PRODUCT_IS_NO_EXIST.getMessage() + productName)
+                );
+    }
+
     public void savePromotions(Promotions promotions) {
         this.promotions = promotions;
     }
 
-    public Promotions getPromotions() {
-        return promotions;
+    public List<Promotion> getPromotions() {
+        return promotions.getPromotions();
+    }
+
+    public Promotion findPromotionByProductName(String productName) {
+        Product findProduct = products.findProductByName(productName);
+        String promotionName = findProduct.getPromotion();
+
+        if (promotionName == null) {
+            return null;
+        }
+        return promotions.getPromotions().stream()
+                .filter(promotion -> promotion.getName().equalsIgnoreCase(promotionName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void updateProductInventory(List<OrderItem> orderItems) {
+        for (OrderItem item : orderItems) {
+            Product product = products.findProductByName(item.getProductName());
+            product.reduceGeneralQuantity(item.getGeneralQuantity());
+            product.reducePromotionQuantity(item.getPromotionQuantity());
+            products.updateProduct(product);
+        }
     }
 }
